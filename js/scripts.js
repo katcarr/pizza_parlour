1d239e75ee997643dabd58921063a95d36720004
katcarr <kathrynelizabethcarr@gmail.com>
2015-02-20 12:00:16 -0800

    Changed type property to kind property to avoid javascript keyword

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ var Pizza = {
       slices = this.diameter - 5;
     }
 
-    if(this.type !== "cheese" && this.diameter >= 6){
+    if(this.kind !== "cheese" && this.diameter >= 6){
       slices -= 2;
     }
 
@@ -17,3 +17,23 @@ var Pizza = {
   }
 
 };
+
+
+// $(function(){
+//
+//   $("pizza-order").submit(function(event){
+//
+//     var size = parseInt($("#size").val());
+//     var  = $("#type").val();
+//
+//     var pizza = Object.create(Pizza);
+//     pizza.diameter = size;
+//     pizza. = type
+//
+//     var slices =
+//
+//     event.preventDefault();
+//   });
+//
+//
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -2,63 +2,63 @@ describe("Pizza", function(){
   describe("slices", function(){
     it("returns 2 if cheese pizza and diameter is 4 inches", function(){
       var  testPizza = Object.create(Pizza);
-      testPizza.type = "cheese";
+      testPizza.kind = "cheese";
       testPizza.diameter = 4;
       expect(testPizza.slices()).to.equal(2);
     });
 
     it("returns 2 if cheese pizza and diameter is 5 inches", function(){
       var  testPizza = Object.create(Pizza);
-      testPizza.type = "cheese";
+      testPizza.kind = "cheese";
       testPizza.diameter = 5;
       expect(testPizza.slices()).to.equal(2);
     });
 
     it("returns 2 if pepperoni pizza and diameter is 4 inches", function(){
       var  testPizza = Object.create(Pizza);
-      testPizza.type = "pepperoni";
+      testPizza.kind = "pepperoni";
       testPizza.diameter = 4;
       expect(testPizza.slices()).to.equal(2);
     });
 
     it("returns 4 if cheese pizza and diameter is 6 inches", function(){
       var  testPizza = Object.create(Pizza);
-      testPizza.type = "cheese";
+      testPizza.kind = "cheese";
       testPizza.diameter = 6;
       expect(testPizza.slices()).to.equal(4);
     });
 
     it("returns 2 if pepperoni pizza and diameter is 6 inches", function(){
       var  testPizza = Object.create(Pizza);
-      testPizza.type = "pepperoni";
+      testPizza.kind = "pepperoni";
       testPizza.diameter = 6;
       expect(testPizza.slices()).to.equal(2);
     });
 
     it("returns 5 less than the diameter when diameter is greater than 16", function(){
       var  testPizza = Object.create(Pizza);
-      testPizza.type = "cheese";
+      testPizza.kind = "cheese";
       testPizza.diameter = 17;
       expect(testPizza.slices()).to.equal(12);
     });
 
     it("if  5 less than the diameter is odd returns 6 less when diameter is greater than 16", function(){
       var  testPizza = Object.create(Pizza);
-      testPizza.type = "cheese";
+      testPizza.kind = "cheese";
       testPizza.diameter = 18;
       expect(testPizza.slices()).to.equal(12);
     });
 
     it("returns 7 less than the diameter when diameter is greater than 16 and is pepporoni", function(){
       var  testPizza = Object.create(Pizza);
-      testPizza.type = "pepporoni";
+      testPizza.kind = "pepporoni";
       testPizza.diameter = 17;
       expect(testPizza.slices()).to.equal(10);
     });
 
     it("if  7 less than the diameter is odd returns 8 less when diameter is greater than 16", function(){
       var  testPizza = Object.create(Pizza);
-      testPizza.type = "pepporoni";
+      testPizza.kind = "pepporoni";
       testPizza.diameter = 18;
       expect(testPizza.slices()).to.equal(10);
     });
