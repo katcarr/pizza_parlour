36627ac504c044da845b8e9baf0a3ab46f215553
katcarr <kathrynelizabethcarr@gmail.com>
2015-02-20 11:12:29 -0800

    Add specs for larger pizzas

--- spec/specs.js
+++ spec/specs.js
@@ -6,7 +6,7 @@ describe("Pizza", function(){
       testPizza.diameter = 4;
       expect(testPizza.slices()).to.equal(2);
     });
-    
+
     it("returns 2 if cheese pizza and diameter is 5 inches", function(){
       var  testPizza = Object.create(Pizza);
       testPizza.type = "cheese";
@@ -33,6 +33,34 @@ describe("Pizza", function(){
       testPizza.type = "pepperoni";
       testPizza.diameter = 6;
       expect(testPizza.slices()).to.equal(2);
+    });
+
+    it("returns 5 less than the diameter when diameter is greater than 16", function(){
+      var  testPizza = Object.create(Pizza);
+      testPizza.type = "cheese";
+      testPizza.diameter = 17;
+      expect(testPizza.slices()).to.equal(12);
+    });
+
+    it("if  5 less than the diameter is odd returns 6 less when diameter is greater than 16", function(){
+      var  testPizza = Object.create(Pizza);
+      testPizza.type = "cheese";
+      testPizza.diameter = 18;
+      expect(testPizza.slices()).to.equal(12);
+    });
+
+    it("returns 7 less than the diameter when diameter is greater than 16 and is pepporoni", function(){
+      var  testPizza = Object.create(Pizza);
+      testPizza.type = "pepporoni";
+      testPizza.diameter = 17;
+      expect(testPizza.slices()).to.equal(10);
+    });
+
+    it("if  7 less than the diameter is odd returns 8 less when diameter is greater than 16", function(){
+      var  testPizza = Object.create(Pizza);
+      testPizza.type = "pepporoni";
+      testPizza.diameter = 18;
+      expect(testPizza.slices()).to.equal(10);
     });
 
 
